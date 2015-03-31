6aed8f967f7a57cdaa7a7538726feccfb8c6abbb
Benjamin Fernandes <benjamin@datadoghq.com>
2015-03-31 14:05:42 -0400

    Make the versioning compatible with local identifier

--- markdown/__version__.py
+++ markdown/__version__.py
@@ -2,15 +2,17 @@
 # markdown/__version__.py
 #
 # version_info should conform to PEP 386
-# (major, minor, micro, alpha/beta/rc/final, #)
+# (major, minor, micro, alpha/beta/rc/final, #, optional local identifier)
 # (1, 1, 2, 'alpha', 0) => "1.1.2.dev"
 # (1, 2, 0, 'beta', 2) => "1.2b2"
-version_info = (2, 6, 1, 'final', 0)
+# (2, 6, 1, 'final', 0) => "2.6.1"
+# (2, 6, 1, 'final', 0, 'dd.1') => "2.6.1+dd.1"
+version_info = (2, 6, 1, 'final', 0, '')
 
 
 def _get_version():
     " Returns a PEP 386-compliant version number from version_info. "
-    assert len(version_info) == 5
+    assert len(version_info) in [5, 6]
     assert version_info[3] in ('alpha', 'beta', 'rc', 'final')
 
     parts = 2 if version_info[2] == 0 else 3
@@ -23,6 +25,9 @@ def _get_version():
     elif version_info[3] != 'final':
         mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'c'}
         sub = mapping[version_info[3]] + str(version_info[4])
+
+    if len(version_info) == 6 and version_info[5]:
+        sub += "+" + str(version_info[5])
 
     return str(main + sub)
 
